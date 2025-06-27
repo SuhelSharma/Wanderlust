3e9b503831bcf793885fb8eb8da4482236e99987
SuhelSharma <sharmasuhel80@gmail.com>
2025-06-27 13:32:34 +0530

    Updated features / fixed bugs

--- init/data.js
+++ init/data.js
@@ -38,7 +38,7 @@ const sampleListings = [
     {
       title: "Historic Villa in Tuscany",
       description:
-        "Experience the charm of Tuscany in this beautifully restored villa. Explore the rolling hills and vineyards.",
+        " the charm of Tuscany in this beautifully restored villa. Explore the rolling hills and vineyards.",
       image: {
         filename: "listingimage",
         url: "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
